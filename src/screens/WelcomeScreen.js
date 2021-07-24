8aef38fddb83a7e863634530dfa61b8116637f0c
John Khachian <jaksawesome10@gmail.com>
2021-07-24 19:56:55 -0400

    welcome

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const WelcomeScreen = () => {
-    return (<Text>Welcome</Text>);
+    return (
+        <View style={styles.container}>
+            <Text>Welcome</Text>
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+    }
+});
 
 export default WelcomeScreen;

637a7f0880f0917d1b67a232a5f6c1b94259419d
John Khachian <jaksawesome10@gmail.com>
2021-08-02 20:09:06 -0400

    Finally got the stubborn back button working

--- src/screens/authentication/ResetPasswordScreen.js
+++ src/screens/authentication/ResetPasswordScreen.js
@@ -1,23 +1,41 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import {
+    SafeAreaView,
+    StyleSheet,
+    StatusBar,
+    View,
+    TouchableWithoutFeedback,
+    Keyboard,
+    TouchableOpacity,
+    Image
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import {  Input, Button, Text } from './../../components/froyo-elements';
 
-const ResetPasswordScreen = () => {
+const ResetPasswordScreen = ({ navigation }) => {
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <SafeAreaView style={styles.container}>
-                <View style={styles.form}>
-                    <Text style={styles.header} >Reset Password</Text>
-                    <Input style={styles.input} placeholder='Email' />
-                    <Button
-                        buttonStyle={styles.submit}
-                        title='Reset Password'
-                        type='primary'
-                        color='#41CA99'
-                        textColor='white'
-                    />
+            <View style={{flex: 1}}>
+                <SafeAreaView>
+                    <StatusBar backgroundColor="#F2F2F2" barStyle="dark-content" />
+                </SafeAreaView>
+                <TouchableOpacity style={styles.back} onPress={() => navigation.navigate('SignIn')}>
+                <Ionicons name="arrow-back" size={52} color="black" />
+                </TouchableOpacity>
+                <View style={styles.container} forceInset={{ top: 'always' }}>
+                    <View style={styles.form}>
+                        <Text style={styles.header} >Reset Password</Text>
+                        <Input style={styles.input} placeholder='Email' />
+                        <Button
+                            buttonStyle={styles.submit}
+                            title='Reset Password'
+                            type='primary'
+                            color='#41CA99'
+                            textColor='white'
+                        />
+                    </View>
                 </View>
-            </SafeAreaView>
+            </View>
         </TouchableWithoutFeedback>
     );
 };
@@ -30,10 +48,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'center'
     },
     form: {
-        paddingBottom: 100,
+        paddingBottom: 200,
         alignItems: 'center',
     },
     header: {
@@ -47,6 +65,9 @@ const styles = StyleSheet.create({
     submit: {
         width: 300,
         margin: 20
+    },
+    back: {
+        margin: 15
     }
 });
 

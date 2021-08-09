01025b211429b1b29e4b78cdf730d8c7c77b3644
John Khachian <jaksawesome10@gmail.com>
2021-08-09 14:23:07 -0400

    guest icon

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text } from './froyo-elements';
+import { Image } from 'react-native';
 
 const Post = ({ author, age, text }) => {
     return (
         <View style={styles.post}>
-            <Text>{author}</Text>
-            <Text>{age}</Text>
+            <Text>
+                <Image source={require('../../assets/icons/guest.png')}/>
+                <Text>{author}</Text>
+                <Text>{age}</Text>
+            </Text>
             <Text>{text}</Text>
         </View>
     );

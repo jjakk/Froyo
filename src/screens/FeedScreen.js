83274eab62e41115525fc6b2de8d942b986f8370
John Khachian <jaksawesome10@gmail.com>
2021-08-10 10:21:17 -0400

    one less post

--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -52,16 +52,6 @@ const FeedScreen = () => {
                     Never gonna say goodbye
                     Never gonna tell a lie and hurt you'
                 />
-                <Post
-                    author='Rick Astley'
-                    age='4y'
-                    text='Never gonna give you up
-                    Never gonna let you down
-                    Never gonna run around and desert you
-                    Never gonna make you cry
-                    Never gonna say goodbye
-                    Never gonna tell a lie and hurt you'
-                />
             </ScrollView>
         </SafeAreaView>
     );

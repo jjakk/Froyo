ca8d492e7bb4448cfcc9682f60b2070ce1bbd7a6
John Khachian <jaksawesome10@gmail.com>
2021-11-03 21:13:46 -0400

    comment action bar

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import { Text } from '../components/froyo-elements';
+// Icons
+import MoreOptionsIcon from '../../assets/icons/MoreSettings.svg';
+import ReplyIcon from '../../assets/icons/Reply.svg';
+import LikeIcon from '../../assets/icons/Like-Outline.svg';
+import DislikeIcon from '../../assets/icons/Dislike-Outline.svg';
+
+const ACTION_SIZE = 25;
+const ACTION_COLOR = 'black';
 
 const Comment = (props) => {
     
@@ -11,7 +19,26 @@ const Comment = (props) => {
 
     return (
         <View style={[styles.comment, style]}>
-            <Text>{text}</Text>
+            <Text style={styles.body}>{text}</Text>
+            <View style={styles.actions}>
+                <TouchableWithoutFeedback>
+                    <MoreOptionsIcon style={styles.moreOptions} width={20} height={20} color={ACTION_COLOR} />
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback>
+                    <View style={styles.reply}>
+                        <ReplyIcon style={styles.replyIcon} width={20} height={20} color={ACTION_COLOR} />
+                        <Text style={styles.replyText}>Reply</Text>
+                    </View>
+                </TouchableWithoutFeedback>
+                <View style={styles.likeness}>
+                    <TouchableWithoutFeedback>
+                        <LikeIcon width={ACTION_SIZE} height={ACTION_SIZE} color={ACTION_COLOR} />
+                    </TouchableWithoutFeedback>
+                    <TouchableWithoutFeedback>
+                        <DislikeIcon style={styles.dislike} width={ACTION_SIZE} height={ACTION_SIZE} color={ACTION_COLOR} />
+                    </TouchableWithoutFeedback>
+                </View>
+            </View>
             {/*
                 Like button
                 Dislike button
@@ -25,8 +52,37 @@ const Comment = (props) => {
 const styles = StyleSheet.create({
     comment: {
         backgroundColor: 'white',
-        marginBottom: 5
+        marginBottom: 5,
+        padding: 15
     },
+    body: {
+        marginBottom: 15
+    },
+    // Action bar
+    actions: {
+        flexDirection: 'row',
+        alignSelf: 'flex-end',
+        alignItems: 'center'
+    },
+    // Reply
+    reply: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 15
+    },
+    replyText: {
+        fontSize: 20,
+        marginLeft: 5,
+    },
+    // Like/Dislike
+    likeness: {
+        flexDirection: 'row',
+        marginLeft: 15
+    },
+    dislike: {
+        marginLeft: 5,
+        marginTop: 5
+    }
 });
 
 export default Comment

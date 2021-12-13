4b2790c832d0cfc92d66692c356b26e62327693b
John Khachian <jaksawesome10@gmail.com>
2021-12-13 11:35:05 -0500

    search screen ScreenContainer

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,41 +1,23 @@
 import React, { useContext } from 'react';
 import {
     StyleSheet,
-    TouchableWithoutFeedback,
-    Keyboard,
     View
 } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Context as PostContext } from '../context/PostContext';
-import { Context as AuthContext } from '../context/AuthContext';
+import ScreenContainer from '../components/ScreenContainer';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
-    const { searchPosts, state: { posts } } = useContext(PostContext);
-    const { searchUsers, state: { users } } = useContext(AuthContext);
 
     return (
-        <SafeAreaView style={styles.container}>
-            <TouchableWithoutFeedback
-                style={styles.container}
-                onPress={Keyboard.dismiss}
-            >
-                <View style={styles.container}>
-                    <SearchBar onSearch={() => {}} />
-                    <View style={styles.results}></View>
-                </View>
-            </TouchableWithoutFeedback>
-        </SafeAreaView>
+        <ScreenContainer>
+            <SearchBar onSearch={() => {}} />
+            <View style={styles.results}></View>
+        </ScreenContainer>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
-    results: {
-
-    }
+    
 });
 
 export default SearchScreen;

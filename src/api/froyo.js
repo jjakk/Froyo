dce994fc67a96434ffbea6305f08d85b78304e2a
John Khachian <jaksawesome10@gmail.com>
2022-01-11 22:38:10 -0500

    Security 100

--- src/api/froyo.js
+++ src/api/froyo.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const instance =  axios.create({
-    baseURL: 'http://api.froyo.social'
+    baseURL: 'https://api.froyo.social'
 });
 
 instance.interceptors.request.use(

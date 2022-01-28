9a9de1e20921f4695897260cb6daca7b9ec1282a
John Khachian <jaksawesome10@gmail.com>
2022-01-28 12:45:48 -0500

    empty message text color improvement

--- src/components/messages/EmptyMessage.js
+++ src/components/messages/EmptyMessage.js
@@ -2,25 +2,17 @@ import React from 'react';
 // Components
 import { View, StyleSheet } from 'react-native';
 import { Text } from '../froyo-elements';
-// Context
-import { useSettings } from '../../context/SettingsContext';
-// Constants
-import { colors } from '../../constants/constants';
 
 const EmptyMessage = ({ style, subheaderText }) => {
-    const { state: { theme } } = useSettings();
-    const darkModeEnabled = theme === 'dark';
 
     return (
         <View style={[styles.container, style]}>
             <Text style={[
                 styles.text,
-                darkModeEnabled ? darkModeStyles.text : null,
                 styles.header
             ]}>Nothing to show</Text>
             <Text style={[
                 styles.text,
-                darkModeEnabled ? darkModeStyles.text : null,
                 styles.subheader
             ]}>{subheaderText}</Text>
         </View>
@@ -41,12 +33,6 @@ const styles = StyleSheet.create({
     subheader: {
         fontSize: 18
     }
-});
-
-const darkModeStyles = StyleSheet.create({
-    text: {
-        color: colors.WHITE
-    },
 });
 
 export default EmptyMessage;

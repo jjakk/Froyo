16ea48e19bf021d214531b0654d5ec0f2929e087
John Khachian <jaksawesome10@gmail.com>
2022-03-07 18:15:02 -0500

    alert for now

--- src/helperFunctions/confirmAlert.js
+++ src/helperFunctions/confirmAlert.js
@@ -1,7 +1,7 @@
 import { Alert } from 'react-native';
 
 
-const confirmAlert = (message, callback) => {
+const confirmAlert = (message, callback=()=>{}) => {
     Alert.alert(
         message.title,
         message.subtitle,
--- src/screens/meetup/MeetupFeedScreen.js
+++ src/screens/meetup/MeetupFeedScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Alert } from 'react-native';
 // Components
 import ScreenContainer from '../../components/ScreenContainer';
 import Header from '../../components/Header';
@@ -7,12 +8,31 @@ import MeetupList from '../../components/meetups/MeetupList';
 import Plus from '../../../assets/icons/Plus.svg';
 
 const MeetupFeedScreen = () => {
+    const onCreateMeetup = () => {
+        Alert.alert(
+            'This feature has not been implemented yet.',
+            null,
+            [
+                {
+                    text: "Ok",
+                    style: "cancel"
+                }
+            ],
+            {
+                cancelable: true
+            }
+        );
+    };
+
     return (
         <ScreenContainer>
             <Header
                 title='Meetups'
                 hideLeftIcon
                 RightIcon={Plus}
+                RightIconProps={{
+                    onPress: onCreateMeetup
+                }}
             />
             <MeetupList
                 emptyMessage="No one in your network's hosting an event at the moment"

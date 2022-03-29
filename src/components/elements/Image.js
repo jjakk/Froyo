9f6a8c0a81be4c3a7ca59e5ee448ea5b1c11e7e0
John Khachian <jaksawesome10@gmail.com>
2022-03-29 10:44:09 -0400

    image component

---
+++ src/components/elements/Image.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Image as DefaultImage } from 'react-native';
+
+const Image = (props) => {
+
+
+    return (
+        <DefaultImage
+            {...props}
+        />
+    );
+};
+
+export default Image;
